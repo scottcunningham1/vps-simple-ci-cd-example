2426c5a4ed290cda472f08680cf36c62d1f0c927
Scott Cunningham <hello@scottcunningham.net>
2023-05-13 20:56:22 +0100

    environment endpoint

--- api/src/index.ts
+++ api/src/index.ts
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.send('API is running!')
 });
 
+app.get('/environment', (req, res) => {
+    res.send(process.env);
+});
+
 app.listen(8000, () => {
   console.log('The application is listening on port 8000...')
 });
