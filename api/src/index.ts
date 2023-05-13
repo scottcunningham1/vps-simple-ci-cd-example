535897e9d63c2b2a7ef6dc76c9d0d5da30da6173
Scott Cunningham <hello@scottcunningham.net>
2023-05-13 20:52:30 +0100

    add gitignore

---
+++ api/src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('API is running!')
+});
+
+app.listen(8000, () => {
+  console.log('The application is listening on port 8000...')
+});
